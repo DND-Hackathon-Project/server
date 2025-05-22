0dcbce10ab9723e4d305987309ade478361d88ba
seungheonlee <lsh2613@naver.com>
2025-05-23 03:05:22 +0900

    first commit

---
+++ src/main/java/com/coderumi/server/CoderumiApplication.java
@@ -0,0 +1,13 @@
+package com.coderumi.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderumiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coderumi/server/CoderumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderumi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

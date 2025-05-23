0720e1c9acf97bfabac1bcb7654cf4e9259705d7
LSH <lsh2613@naver.com>
2025-05-24 04:55:07 +0900

    fix: 충돌 해결

--- src/main/java/com/coderumi/server/repository/PosterCustomRepository.java
+++ src/main/java/com/coderumi/server/repository/PosterCustomRepository.java
@@ -1,13 +1,16 @@
 package com.coderumi.server.repository;
 
 import com.coderumi.server.dto.PosterDto;
+import com.coderumi.server.entity.Poster;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PosterCustomRepository {
 
     List<PosterDto> searchPosters(String region, boolean isSelected);
 
+    Optional<Poster> findHottestPosterByRegion(String region);
 }

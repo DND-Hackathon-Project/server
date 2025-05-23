828474721738f1133065792b6e610bd0b677a549
LSH <lsh2613@naver.com>
2025-05-24 05:58:33 +0900

    feat: 정적 리소스 제공 핸들러 적용

--- src/main/java/com/coderumi/server/common/config/WebConfig.java
+++ src/main/java/com/coderumi/server/common/config/WebConfig.java
@@ -2,10 +2,18 @@ package com.coderumi.server.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
